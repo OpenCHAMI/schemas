8dd3712e236241ea348eee6d6698a661324d7fc5
Ben McDonald <ben.mcdonald@hpe.com>
2025-06-23 13:59:21 -0700

    Switch Actions to a string array to work with magellan

--- schemas/inventory.go
+++ schemas/inventory.go
@@ -54,30 +54,30 @@ type Links struct {
 }
 
 type InventoryDetail struct {
-	URI                  string                 `json:"uri,omitempty"`                  // URI of the BMC
-	UUID                 string                 `json:"uuid,omitempty"`                 // UUID of Node
-	Manufacturer         string                 `json:"manufacturer,omitempty"`         // Manufacturer of the Node
-	SystemType           string                 `json:"system_type,omitempty"`          // System type of the Node
-	Name                 string                 `json:"name,omitempty"`                 // Name of the Node
-	Model                string                 `json:"model,omitempty"`                // Model of the Node
-	Serial               string                 `json:"serial,omitempty"`               // Serial number of the Node
-	BiosVersion          string                 `json:"bios_version,omitempty"`         // Version of the BIOS
-	EthernetInterfaces   []EthernetInterface    `json:"ethernet_interfaces,omitempty"`  // Ethernet interfaces of the Node
-	NetworkInterfaces    []NetworkInterface     `json:"network_interfaces,omitempty"`   // Network interfaces of the Node
-	PowerState           string                 `json:"power_state,omitempty"`          // Power state of the Node
-	ProcessorCount       int                    `json:"processor_count,omitempty"`      // Processors of the Node
-	ProcessorType        string                 `json:"processor_type,omitempty"`       // Processor type of the Node
-	MemoryTotal          float32                `json:"memory_total,omitempty"`         // Total memory of the Node in Gigabytes
-	TrustedModules       []string               `json:"trusted_modules,omitempty"`      // Trusted modules of the Node
-	TrustedComponents    []string               `json:"trusted_components,omitempty"`   // Trusted components of the Chassis
-	Chassis_SKU          string                 `json:"chassis_sku,omitempty"`          // SKU of the Chassis
-	Chassis_Serial       string                 `json:"chassis_serial,omitempty"`       // Serial number of the Chassis
-	Chassis_AssetTag     string                 `json:"chassis_asset_tag,omitempty"`    // Asset tag of the Chassis
-	Chassis_Manufacturer string                 `json:"chassis_manufacturer,omitempty"` // Manufacturer of the Chassis
-	Chassis_Model        string                 `json:"chassis_model,omitempty"`        // Model of the Chassis
-	OdataId              string                 `json:"@odata.id,omitempty"`            // OData ID for the computer system
-	PowerURL             string                 `json:"PowerURL,omitempty"`             // URL for power control
-	PowerControl         []*PowerControl        `json:"PowerControl,omitempty"`         // Power control actions data
-	Actions              *ComputerSystemActions `json:"Actions,omitempty"`              // Actions for the hardware
-	Links                *Links                 `json:"links,omitempty"`                // Links to related resources
+	URI                  string              `json:"uri,omitempty"`                  // URI of the BMC
+	UUID                 string              `json:"uuid,omitempty"`                 // UUID of Node
+	Manufacturer         string              `json:"manufacturer,omitempty"`         // Manufacturer of the Node
+	SystemType           string              `json:"system_type,omitempty"`          // System type of the Node
+	Name                 string              `json:"name,omitempty"`                 // Name of the Node
+	Model                string              `json:"model,omitempty"`                // Model of the Node
+	Serial               string              `json:"serial,omitempty"`               // Serial number of the Node
+	BiosVersion          string              `json:"bios_version,omitempty"`         // Version of the BIOS
+	EthernetInterfaces   []EthernetInterface `json:"ethernet_interfaces,omitempty"`  // Ethernet interfaces of the Node
+	NetworkInterfaces    []NetworkInterface  `json:"network_interfaces,omitempty"`   // Network interfaces of the Node
+	PowerState           string              `json:"power_state,omitempty"`          // Power state of the Node
+	ProcessorCount       int                 `json:"processor_count,omitempty"`      // Processors of the Node
+	ProcessorType        string              `json:"processor_type,omitempty"`       // Processor type of the Node
+	MemoryTotal          float32             `json:"memory_total,omitempty"`         // Total memory of the Node in Gigabytes
+	TrustedModules       []string            `json:"trusted_modules,omitempty"`      // Trusted modules of the Node
+	TrustedComponents    []string            `json:"trusted_components,omitempty"`   // Trusted components of the Chassis
+	Chassis_SKU          string              `json:"chassis_sku,omitempty"`          // SKU of the Chassis
+	Chassis_Serial       string              `json:"chassis_serial,omitempty"`       // Serial number of the Chassis
+	Chassis_AssetTag     string              `json:"chassis_asset_tag,omitempty"`    // Asset tag of the Chassis
+	Chassis_Manufacturer string              `json:"chassis_manufacturer,omitempty"` // Manufacturer of the Chassis
+	Chassis_Model        string              `json:"chassis_model,omitempty"`        // Model of the Chassis
+	OdataId              string              `json:"@odata.id,omitempty"`            // OData ID for the computer system
+	PowerURL             string              `json:"PowerURL,omitempty"`             // URL for power control
+	PowerControl         []*PowerControl     `json:"PowerControl,omitempty"`         // Power control actions data
+	Actions              []string            `json:"actions,omitempty"`              // Actions for the hardware
+	Links                *Links              `json:"links,omitempty"`                // Links to related resources
 }
